ca6bbcf5242e8c2da6bf873824b72cb2672cde7d
kvale17 <kevin.valencia24@gmail.com>
2024-04-29 12:51:01 -0700

    Add gameboard object

--- src/game.js
+++ src/game.js
@@ -1,3 +1,26 @@
+const GameBoard = (function () {
+    const gameBoard =
+        [
+            { row: "a", column: "1", id: "a1", mark: 0 }, { row: "a", column: "2", cell: "a2", mark: 0 }, { row: "a", column: "3", cell: "a3", mark: 0 },
+            { row: "b", column: "1", id: "b1", mark: 0 }, { row: "b", column: "2", cell: "b2", mark: 0 }, { row: "b", column: "3", cell: "b3", mark: 0 },
+            { row: "c", column: "1", id: "c1", mark: 0 }, { row: "c", column: "2", cell: "c2", mark: 0 }, { row: "c", column: "3", cell: "c3", mark: 0 }
+        ];
+
+    const placeMark = (cell, mark) => {
+        let index = gameBoard.findIndex(gameBoard => gameBoard.id == cell);
+
+        if (mark === "X" || mark === "O") {
+            gameBoard[index].mark = mark;
+        }
+        else {
+            console.error("Invalid mark (must be X or O)");
+        }
+    }
+
+    return { gameBoard, placeMark };
+})();
+
+
 function createPlayer(name) {
 
     let score = 0;
@@ -12,4 +35,7 @@ function createPlayer(name) {
 
     return { name, addPoint, getPoints };
 }
+
+
+
 
