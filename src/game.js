14b11f796e21a51291795919da5d356f8ac416f5
kvale17 <kevin.valencia24@gmail.com>
2024-04-29 15:05:21 -0700

    Convert gameflow to module

--- src/game.js
+++ src/game.js
@@ -1,28 +1,23 @@
-const GameBoard = (function () {
-    const gameBoard =
-        [
-            { row: "a", column: "1", cell: "a1", mark: 0 }, { row: "a", column: "2", cell: "a2", mark: 0 }, { row: "a", column: "3", cell: "a3", mark: 0 },
-            { row: "b", column: "1", cell: "b1", mark: 0 }, { row: "b", column: "2", cell: "b2", mark: 0 }, { row: "b", column: "3", cell: "b3", mark: 0 },
-            { row: "c", column: "1", cell: "c1", mark: 0 }, { row: "c", column: "2", cell: "c2", mark: 0 }, { row: "c", column: "3", cell: "c3", mark: 0 }
-        ];
+const GameBoard = (() => {
+    const gameBoard = [
+        { row: "a", column: "1", cell: "a1", mark: 0 }, { row: "a", column: "2", cell: "a2", mark: 0 }, { row: "a", column: "3", cell: "a3", mark: 0 },
+        { row: "b", column: "1", cell: "b1", mark: 0 }, { row: "b", column: "2", cell: "b2", mark: 0 }, { row: "b", column: "3", cell: "b3", mark: 0 },
+        { row: "c", column: "1", cell: "c1", mark: 0 }, { row: "c", column: "2", cell: "c2", mark: 0 }, { row: "c", column: "3", cell: "c3", mark: 0 }
+    ];
 
     const placeMark = (cell, mark, player) => {
-        let index = gameBoard.findIndex(gameBoard => gameBoard.cell == cell);
+        let index = gameBoard.findIndex(cellObj => cellObj.cell === cell);
 
-        if (mark === "X" || mark === "O") {
+        if (index !== -1 && (mark === "X" || mark === "O")) {
             gameBoard[index].mark = mark;
-
             player.addMark();
-        }
-        else {
-            console.error("Invalid mark (must be X or O)");
+        } else {
+            console.error("Invalid mark or cell");
         }
     }
 
     const getEmptyCells = () => {
-        const result = GameBoard.gameBoard.filter(cell => cell.mark === 0);
-
-        return result;
+        return gameBoard.filter(cell => cell.mark === 0);
     }
 
     return { gameBoard, placeMark, getEmptyCells };
@@ -40,7 +35,7 @@ function createPlayer(name) {
     return { name, addMark, getMarks };
 }
 
-function gameFlow() {
+const GameFlow = (() => {
     const getUserInput = () => {
         const input = prompt("What cell do you want to place a mark in ?");
 
@@ -57,6 +52,5 @@ function gameFlow() {
         return computerChoiceCell;
     }
 
-    return { getUserInput, getComputerInput};
-
-}
+    return { getUserInput, getComputerInput };
+})();
